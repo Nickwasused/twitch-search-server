bd1789132c9eed6d822662f7f603472465bf3bd8
Nickwasused <34883496+Nickwasused@users.noreply.github.com>
2022-03-13 16:47:30 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # twitch-search-server
 The reason to create this api was because of this https://discuss.dev.twitch.tv/t/search-by-title-not-possible/33868
+
+# setup
+Install the node packages with npm install.<br />
+Copy the .env.example file to .env and edit it.<br />
+After that launch the server with: node index.js.
---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const axios = require('axios');
+require('dotenv').config()
+const app = express();
+
+let streams = [];
+let client_id = process.env.CLIENT_ID;
+let bearer_auth = process.env.BEARER_AUTH;
+let paginator = "";
+let language = process.env.LANGUAGE;
+let game_id = process.env.GAME_ID;
+
+let streams_url = "https://api.twitch.tv/helix/streams";
+
+async function fetchstreams() {
+    console.log("Starting to fetch Streams!")
+    let fetching = true;
+    let temp_streams = [];
+    let search_params = {
+        first: 100
+    };
+    if (language != "" && language != undefined) {
+        search_params["language"] = language;
+    }
+    if (game_id != "" && game_id != undefined) {
+        search_params["game_id"] = game_id;
+    }
+    while (fetching) {
+        if (paginator != "" && paginator != undefined) {
+            search_params["after"] = paginator;
+        }
+        await Promise.all([
+            axios.get(streams_url, {
+                headers: {
+                    "client-id": client_id,
+                    "Authorization": "Bearer " + bearer_auth,
+                },
+                params: search_params
+            })
+            .then(response => {
+                for (let i=0; i < response.data["data"].length; i++) {
+                    temp_streams.push(response.data["data"][i])
+                }
+                paginator = response.data["pagination"]["cursor"];
+                if (response.data["data"].length == 0) {
+                    fetching = false;
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        ]);
+        console.log(`We have fetched ${temp_streams.length} Streams!`)
+    }
+
+    console.log("Done fetching Streams!")
+    paginator = "";
+    streams = temp_streams;
+    
+    return;
+}
+
+async function startup() {
+    await fetchstreams();
+    setInterval(fetchstreams, 300000);
+}
+
+
+app.get("/search", (request, response) => {
+    response.type('json');
+    let search_title = request.query.title;
+    let search_viewers = request.query.viewers;
+    let search_game = request.query.game;
+    let search_language = request.query.language;
+    let api_response = streams.filter((stream) => {
+        let { title, game_id, language, viewer_count } = stream;
+        if (search_title != undefined && !title.toLowerCase().includes(search_title.toLowerCase())) {
+            return null;
+        } else if (search_viewers != undefined && !viewer_count == search_viewers) {
+            return null;
+        } else if (search_game != undefined && !game_id == search_game) {
+            return null;
+        } else if (search_language != undefined && !language == search_language) {
+            return null;
+        } else {
+            return stream;
+        }
+        
+    });
+    response.send(JSON.stringify(api_response));
+});
+
+app.listen(3000, () => {
+    startup()
+    console.log("Listen on the port 3000...");
+});
