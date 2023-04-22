208c77cef55f8042318f4c1611e56bf395565a01
Nickwasused <github.lix1s@simplelogin.co>
2023-04-22 16:22:18 +0200

    lint and fmt /app

--- app/app.d.ts
+++ app/app.d.ts
@@ -1,34 +1,34 @@
 type Streamer = {
-    id: string;
-    user_id: string;
-    user_login: string;
-    user_name: string;
-    game_id: string;
-    game_name: string;
-    type: string;
-    title: string;
-    viewer_count: number;
-    started_at: string;
-    language: string;
-    thumbnail_url: string;
-    tag_ids: Array<string>;
-    tags: Array<string>;
-    is_mature: boolean;
-}
+	id: string;
+	user_id: string;
+	user_login: string;
+	user_name: string;
+	game_id: string;
+	game_name: string;
+	type: string;
+	title: string;
+	viewer_count: number;
+	started_at: string;
+	language: string;
+	thumbnail_url: string;
+	tag_ids: Array<string>;
+	tags: Array<string>;
+	is_mature: boolean;
+};
 
 type Twitch_Api_Streams = {
-    data: Array<Streamer>,
-    pagination: pagination
-}
+	data: Array<Streamer>;
+	pagination: pagination;
+};
 
 type pagination = {
-    cursor?: string
-}
+	cursor?: string;
+};
 
 interface Filter {
-    [key: string]: string[];
+	[key: string]: string[];
 }
 
 interface Params {
-    [key: string]: string;
+	[key: string]: string;
 }
