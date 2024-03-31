03f5a8eeb297729c96502448ffb61ed3b262e4d9
Nickwasused <github.lix1s@simplelogin.co>
2024-03-31 19:02:02 +0200

    change background task

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python3
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.encoders import jsonable_encoder
 from contextlib import asynccontextmanager
 from fetch import Handler, Streamer
-from fastapi.encoders import jsonable_encoder
+from threading import Thread
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-import asyncio
 import uvicorn
+import sched
+import time
 
 handler = Handler()
+s = sched.scheduler(time.time, time.sleep)
 
 
-async def update_streamers(wait: int = 300):
-    while True:
-        handler.get_streamers()
-        await asyncio.sleep(wait)
+def update_streamers(sc: sched.scheduler, wait: float = 300):
+    handler.get_streamers()
+    sc.enter(wait, 1, update_streamers, (sc, wait,))
+    sc.run()
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    """background task starts at startup
-    https://stackoverflow.com/a/78046933
-    """
-    _streamer_task = asyncio.create_task(update_streamers())
+    """background task starts at startup https://stackoverflow.com/a/75266289"""
+    thread = Thread(target=update_streamers, kwargs=dict(sc=s, wait=300))
+    thread.start()
     yield
 
 
